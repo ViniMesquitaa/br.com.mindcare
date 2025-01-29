206b8ac62fd03af3f3d27c22bfdcec280fb5393c
pedrolucazx <pedrolucastrainee@gmail.com>
2025-01-29 10:18:46 -0300

    feat: create tabs component

---
+++ src/components/Tabs/index.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import "./styles.css";
+
+export default function Tabs({ tabs }) {
+  const [activeTab, setActiveTab] = useState(tabs[0]?.name || "");
+
+  return (
+    <div className="tabs-container">
+      <div className="tabs">
+        {tabs.map((tab) => (
+          <button
+            key={tab.name}
+            className={activeTab === tab.name ? "active" : ""}
+            onClick={() => setActiveTab(tab.name)}
+          >
+            {tab.name}
+          </button>
+        ))}
+      </div>
+
+      <div className="tab-content">
+        {tabs.map(
+          (tab) =>
+            activeTab === tab.name && (
+              <div key={tab.name} className="tab-pane">
+                {tab.form}
+              </div>
+            )
+        )}
+      </div>
+    </div>
+  );
+}
